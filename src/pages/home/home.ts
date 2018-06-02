139cd8ff59d46dbf27f593e743347735f3cbd38a
Charles Albert <charles.ccomp@gmail.com>
2018-06-02 02:52:09 -0300

    implemented item addtion

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,28 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ModalController, NavController } from 'ionic-angular';
+import { AddItemPage } from '../add-item/add-item';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  public items;
+  public items = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
-  }
-
-  ionViewDidLoad(){
-    this.items = [
-      {title: 'hi1', description: 'test1'},
-      {title: 'hi2', description: 'test2'},
-      {title: 'hi3', description: 'test3'},
-    ]
   }
 
   addItem(){
+    let addModal = this.modalCtrl.create(AddItemPage);
+    addModal.onDidDismiss((item) => {
+      if(item){
+        this.saveItem(item);
+      }
+    })
+    addModal.present();
+  }
 
+  saveItem(item){
+    this.items.push(item);
   }
 
   viewItem(){
